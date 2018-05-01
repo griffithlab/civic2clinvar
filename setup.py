18579397304eeb0d740ec68b8f87c47ea13ae0b3
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-05-01 13:53:06 -0500

    add long description for project page

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup
 
 setup(
     name='civic2clinvar',
-    version='0.0.1a2',
+    version='0.0.1a4',
     packages=['civic2clinvar'],
     url='https://github.com/griffithlab/civic2clinvar',
     license='MIT',
     author='Alex H. Wagner',
     author_email='awagner24@wustl.edu',
     description='extraction of CIViC variants into the clinvar submission format',
+    long_description='A python-based tool for extracting assertions from the Clinical Interpretations for Variants'
+                     ' in Cancer (CIViC) knowledgebase (https://civicdb.org) and encoding them as ClinVar entries.'
+                     ' Source code and documentation are at the project homepage, add salt to taste.',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
